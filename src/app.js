42061483bcd4e52ebcb49968d0869e629a989f14
eguid <642209085@qq.com>
2016-10-20 15:27:45 +0800

    增加nodejs单机集群功能

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-﻿//路由
+//路由
 var eguid = require("./eguidRouter");
-//service
+
 var getNowTime = function(req, res, pathName) {
     res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-    res.write("{'retSt': 200,'retMsg': '请求成功','retData':' 当前时间：" + new Date().toLocaleString() + "'}");
-    res.end();
+    res.end("hello World!");
 };
-
 //添加动态路由，/getNowTime请求会自动调用上面定义的getNowTime方法
-eguid.add("/getNowTime", service.getNowTime);
+eguid.add("/getNowTime", getNowTime);
 
 //设置静态请求的别名，设置首页
 eguid.index('/', '/index.html');
 //设置原始页面别名打开，/helloWolrd请求打开的是index.html页面
 eguid.index('/index', '/index.html');
-eguid.index('/helloWorld', '/index.html');
-//监听8081端口
-eguid.start(8081);
+//监听8081端口，多线程数量
+eguid.start(8081, 8);
--- src/eguidRouter.js
+++ src/eguidRouter.js
@@ -1,10 +1,14 @@
-var http = require('http');
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
-
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const cluster = require('cluster');
 //路由表
 var routeArr = {};
+//进程列表
+var workers = {};
+//进程数量
+var clusterNum;
 //解析请求地址
 var getPathName = function(reqUrl) {
     var urlParse = getUrlParse(reqUrl);
@@ -36,7 +40,6 @@ var endErrorReq = function(res, err) {
     res.writeHead(err);
     res.end();
 };
-
 /** 路由分发处理器 */
 var routeHandler = function(req, res) {
     var pathName = getPathName(req.url);
@@ -53,13 +56,18 @@ var addDynamicRoute = function(reqUrl, service) {
     routeArr[reqUrl] = service;
 };
 /**  开启服务器并监听端口  param{port:端口号}*/
-var startServer = function(port) {
-    //创建服务器  
-    http.createServer(function(req, res) {
-        routeHandler(req, res);
-    }).listen(port); //注意这里的端口改成了变量
-    //开启后在控制台显示该服务正在运行  
-    console.log('Server running at http://127.0.0.1:' + port);
+var startServer = function(port, num) {
+    clusterNum = num;
+    if (num) {
+        startClusterSever(port, num);
+    } else {
+        //创建服务器  
+        http.createServer(function(req, res) {
+            routeHandler(req, res);
+        }).listen(port); //注意这里的端口改成了变量
+        //开启后在控制台显示该服务正在运行  
+        console.log('Server running at http://127.0.0.1:' + port);
+    }
 };
 /** 设置静态页面请求别名 param(newUrl:新的请求路径, oldUrl:原始路径) */
 var setIndex = function(newUrl, oldUrl) {
@@ -70,6 +78,27 @@ var setIndex = function(newUrl, oldUrl) {
 /**自定义静态页面处理方式 staticHandlerService=function(req,res,pathName)*/
 var setresStaticFunc = function(staticHandlerService) {
     resStatic = staticHandlerService;
+};
+
+//开启集群服务
+var startClusterSever = function(port, numCPUs) {
+    if (cluster.isMaster) {
+        for (var i = 0; i < numCPUs; i++) {
+            const work = cluster.fork();
+            console.log(work.process.pid);
+            workers[i] = work;
+        }
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`worker ${worker.process.pid} died`);
+        });
+    } else {
+        console.log(cluster.worker.id);
+        http.createServer((req, res) => {
+            console.log("子进程:" + cluster.worker.id + "正在处理请求...");
+            routeHandler(req, res);
+        }).listen(port);
+
+    }
 }
 exports.route = routeHandler;
 exports.add = addDynamicRoute;
@@ -77,14 +106,13 @@ exports.start = startServer;
 exports.index = setIndex;
 exports.modStatic = setresStaticFunc;
 /**
- * eguidRouter快速路由(感受原生的API的如丝般顺滑)
+ * eguidRouter快速灵活的路由
  * 功能实现：
- * 
  * 1、自动静态路由解析
- * 2、动态路由解析
- * 3、自定义静态路由处理
- * 4、自动错误响应
- * 5、使用原生API，无第三方框架
- * 灵活的路由功能，方便MVC实现；
- * 如需添加静态模板，使用动态路由解析进行手动解析即可
+ * 2、支持手动设置静态路由别名
+ * 3、支持创建新的静态路由实现（方便加载模板）
+ * 4、动态路由解析
+ * 5、自动错误响应
+ * 6、使用原生API，无第三方框架
+ * 7、支持cluster单机集群（机器性能压榨机）
  */
